34675537110ed5e5dd7777ae9f2de74a4781069c
FortuneCat0v0 <1142094319@qq.com>
2024-03-12 15:02:34 +0800

    .

--- Unity/Assets/Scripts/Hotfix/Share/Zero/Item/ItemFactory.cs
+++ Unity/Assets/Scripts/Hotfix/Share/Zero/Item/ItemFactory.cs
@@ -1,22 +1,23 @@
-﻿namespace ET;
-
-public static class ItemFactory
+﻿namespace ET
 {
-    public static Item Create(Scene root, ItemInfo itemInfo)
+    public static class ItemFactory
     {
-        Item item = root.AddChildWithId<Item>(itemInfo.Id);
-        item.FromMessage(itemInfo);
-        return item;
-    }
+        public static Item Create(Scene root, ItemInfo itemInfo)
+        {
+            Item item = root.AddChildWithId<Item>(itemInfo.Id);
+            item.FromMessage(itemInfo);
+            return item;
+        }
 
-    public static Item Create(Scene root, int configId)
-    {
-        Item item = root.AddChild<Item, int>(configId);
-        AddComponentByItemType(item);
-        return item;
-    }
+        public static Item Create(Scene root, int configId)
+        {
+            Item item = root.AddChild<Item, int>(configId);
+            AddComponentByItemType(item);
+            return item;
+        }
 
-    public static void AddComponentByItemType(Item item)
-    {
+        public static void AddComponentByItemType(Item item)
+        {
+        }
     }
 }
