1dd4ec401ab28eb487215fe5e9f24a063f6bffef
FortuneCat0v0 <1142094319@qq.com>
2024-03-15 12:25:55 +0800

    Main界面，一些按钮

--- Unity/Assets/Scripts/HotfixView/Client/Zero/UI/UIMain/UIMainComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Zero/UI/UIMain/UIMainComponentSystem.cs
@@ -13,7 +13,75 @@ namespace ET.Client
         {
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
 
-            self.UIJoystickComponent = self.AddComponent<UIJoystickComponent, GameObject>(rc.Get<GameObject>("UIJoystick"));
+            self.LBShrinkBtn = rc.Get<GameObject>("LBShrinkBtn");
+            self.LBBtns = rc.Get<GameObject>("LBBtns");
+            self.BagBtn = rc.Get<GameObject>("BagBtn");
+            self.PetBtn = rc.Get<GameObject>("PetBtn");
+            self.SkillBtn = rc.Get<GameObject>("SkillBtn");
+            self.TaskBtn = rc.Get<GameObject>("TaskBtn");
+            self.SocialBtn = rc.Get<GameObject>("SocialBtn");
+            self.AchievementBtn = rc.Get<GameObject>("AchievementBtn");
+            self.UIJoystick = rc.Get<GameObject>("UIJoystick");
+
+            self.LBShrinkBtn.GetComponent<Button>().AddListener(self.OnLBShrinkBtn);
+            self.BagBtn.GetComponent<Button>().AddListenerAsync(self.OnBagBtn);
+            self.PetBtn.GetComponent<Button>().AddListenerAsync(self.OnPetBtn);
+            self.SkillBtn.GetComponent<Button>().AddListenerAsync(self.OnSkillBtn);
+            self.TaskBtn.GetComponent<Button>().AddListenerAsync(self.OnTaskBtn);
+            self.SocialBtn.GetComponent<Button>().AddListenerAsync(self.OnSocialBtn);
+            self.AchievementBtn.GetComponent<Button>().AddListenerAsync(self.OnAchievementBtn);
+
+            self.UIJoystickComponent = self.AddComponent<UIJoystickComponent, GameObject>(self.UIJoystick);
+        }
+
+        private static void OnLBShrinkBtn(this UIMainComponent self)
+        {
+            if (self.LBBtns.activeSelf)
+            {
+                self.LBBtns.SetActive(false);
+                self.LBShrinkBtn.GetComponent<RectTransform>().localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                self.LBBtns.SetActive(true);
+                self.LBShrinkBtn.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            }
+        }
+
+        private static async ETTask OnBagBtn(this UIMainComponent self)
+        {
+            Log.Info("背包界面暂未开放");
+            await ETTask.CompletedTask;
+        }
+
+        private static async ETTask OnPetBtn(this UIMainComponent self)
+        {
+            Log.Info("宠物界面暂未开放");
+            await ETTask.CompletedTask;
+        }
+
+        private static async ETTask OnSkillBtn(this UIMainComponent self)
+        {
+            Log.Info("技能界面暂未开放");
+            await ETTask.CompletedTask;
+        }
+
+        private static async ETTask OnTaskBtn(this UIMainComponent self)
+        {
+            Log.Info("任务界面暂未开放");
+            await ETTask.CompletedTask;
+        }
+
+        private static async ETTask OnSocialBtn(this UIMainComponent self)
+        {
+            Log.Info("技能界面暂未开放");
+            await ETTask.CompletedTask;
+        }
+
+        private static async ETTask OnAchievementBtn(this UIMainComponent self)
+        {
+            Log.Info("成就界面暂未开放");
+            await ETTask.CompletedTask;
         }
     }
 }
--- Unity/Assets/Scripts/ModelView/Client/Zero/UI/UIMain/UIMainComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/Zero/UI/UIMain/UIMainComponent.cs
@@ -5,6 +5,16 @@ namespace ET.Client
     [ComponentOf(typeof(UI))]
     public class UIMainComponent : Entity, IAwake
     {
+        public GameObject LBShrinkBtn;
+        public GameObject LBBtns;
+        public GameObject BagBtn;
+        public GameObject PetBtn;
+        public GameObject SkillBtn;
+        public GameObject TaskBtn;
+        public GameObject SocialBtn;
+        public GameObject AchievementBtn;
+        public GameObject UIJoystick;
+
         private EntityRef<UIJoystickComponent> uiJoystickComponent;
 
         public UIJoystickComponent UIJoystickComponent
