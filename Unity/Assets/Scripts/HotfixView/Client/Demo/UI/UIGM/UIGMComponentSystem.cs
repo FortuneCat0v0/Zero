177cafc25f712336239da4a9504a2f00bba410d6
FortuneCat0v0 <1142094319@qq.com>
2024-07-07 22:17:56 +0800

    UIGM

---
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UIGM/UIGMComponentSystem.cs
@@ -0,0 +1,13 @@
+﻿namespace ET.Client
+{
+    [FriendOf(typeof(UIGMComponent))]
+    [EntitySystemOf(typeof(UIGMComponent))]
+    public static partial class UIGMComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this UIGMComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+        }
+    }
+}
