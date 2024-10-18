8266feb3be32896d782940de34b7b786dd9f8a43
FortuneCat0v0 <1142094319@qq.com>
2024-10-18 17:08:45 +0800

    .

--- Unity/Assets/Scripts/Model/Share/Module/Scene/CurrentScenesComponent.cs
+++ Unity/Assets/Scripts/Model/Share/Module/Scene/CurrentScenesComponent.cs
@@ -2,20 +2,10 @@
 {
     // 可以用来管理多个客户端场景，比如大世界会加载多块场景
     [ComponentOf(typeof(Scene))]
-    public class CurrentScenesComponent: Entity, IAwake
+    public class CurrentScenesComponent : Entity, IAwake
     {
         private EntityRef<Scene> scene;
 
-        public Scene Scene
-        {
-            get
-            {
-                return this.scene;
-            }
-            set
-            {
-                this.scene = value;
-            }
-        }
+        public Scene Scene { get => this.scene; set => this.scene = value; }
     }
 }
