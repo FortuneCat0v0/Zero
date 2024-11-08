0696e4fb4542087dc1ef183ab930a0096d202b6b
FortuneCat0v0 <1142094319@qq.com>
2024-11-08 17:11:31 +0800

    .

--- Unity/Assets/Scripts/Editor/Tool/SceneTextureReferenceFinder.cs
+++ Unity/Assets/Scripts/Editor/Tool/SceneTextureReferenceFinder.cs
@@ -15,7 +15,7 @@ namespace ET.Client
         private static readonly string scenesPath = "Assets/Bundles/Scenes";
         private static readonly string outputPath = "Assets/场景中引用的贴图.txt";
 
-        [MenuItem("Tools/场景中引用的贴图")]
+        [MenuItem("Tools/检测场景中引用的贴图")]
         public static void FindTextureReferences()
         {
             Log.Debug("查找开始！");
@@ -64,8 +64,7 @@ namespace ET.Client
                 result.Add($"{sceneName}:");
                 foreach (var (texture, size) in texturesInScene)
                 {
-                    result.Add($"  {texture.name} - {FormatBytes(size)}");
-                    // result.Add($"  {texture.name} - {FormatBytes(size)} - Path: {AssetDatabase.GetAssetPath(texture)}");
+                    result.Add($"  {AssetDatabase.GetAssetPath(texture)} - 大小：{FormatBytes(size)} - 尺寸：{texture.width}x{texture.height}");
                 }
 
                 result.Add(""); // 空行分隔场景
