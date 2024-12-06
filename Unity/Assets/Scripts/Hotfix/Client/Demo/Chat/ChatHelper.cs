d8056dfafccdf485ec561ed3c7229484ce387858
FortuneCat0v0 <1142094319@qq.com>
2024-12-06 17:32:54 +0800

    聊天测试-发送信息

---
+++ Unity/Assets/Scripts/Hotfix/Client/Demo/Chat/ChatHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace ET.Client
+{
+    public static class ChatHelper
+    {
+        public static async ETTask<int> RequestSendChat(Scene root, string message)
+        {
+            C2Chat_SendChat request = C2Chat_SendChat.Create();
+            request.ChatMessage = message;
+
+            Chat2C_SendChat response = (Chat2C_SendChat)await root.GetComponent<ClientSenderComponent>().Call(request);
+            return response.Error;
+        }
+    }
+}
--- Unity/Assets/Scripts/HotfixView/Client/Module/GM/GM_Command_Test.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Module/GM/GM_Command_Test.cs
@@ -9,7 +9,7 @@ namespace ET.Client
     //3 命令名称 页面上显示的名字
     //4 命令描述 有描述时显示描述 没有时不显示
     [GM(EGMType.Test, 1, "测试案列", "测试案列描述.....")]
-    public class GM_Test: IGMCommand
+    public class GM_Test : IGMCommand
     {
         public List<GMParamInfo> GetParams()
         {
@@ -29,12 +29,12 @@ namespace ET.Client
         public async ETTask<bool> Run(Scene root, ParamVo paramVo)
         {
             //通过paramvo 可以取出所有参数
-            var paramEnum   = paramVo.Get<EGMParamType>(0);
+            var paramEnum = paramVo.Get<EGMParamType>(0);
             var paramString = paramVo.Get<string>(1);
-            var paramBool   = paramVo.Get<bool>(2);
-            var paramFloat  = paramVo.Get<float>(3);
-            var paramInt    = paramVo.Get<int>(4);
-            var paramLong   = paramVo.Get<long>(5);
+            var paramBool = paramVo.Get<bool>(2);
+            var paramFloat = paramVo.Get<float>(3);
+            var paramInt = paramVo.Get<int>(4);
+            var paramLong = paramVo.Get<long>(5);
 
             //最后根据参数自行处理命令
             Debug.LogError($"参数0: {paramEnum}");
@@ -47,5 +47,131 @@ namespace ET.Client
             await ETTask.CompletedTask;
             return true;
         }
+
+        # region 聊天
+
+        [GM(EGMType.Test, 1, "聊天测试-发送信息")]
+        public class GM_Command_Chat1 : IGMCommand
+        {
+            public List<GMParamInfo> GetParams()
+            {
+                return new()
+                {
+                    new GMParamInfo(EGMParamType.String, "内容")
+                };
+            }
+
+            public async ETTask<bool> Run(Scene root, ParamVo paramVo)
+            {
+                var paramString = paramVo.Get<string>();
+
+                await ChatHelper.RequestSendChat(root, paramString);
+
+                await ETTask.CompletedTask;
+                return true;
+            }
+        }
+
+        #endregion
+
+        # region 背包
+
+        [GM(EGMType.Test, 1, "背包测试-添加道具")]
+        public class GM_Command_Knapsack1 : IGMCommand
+        {
+            public List<GMParamInfo> GetParams()
+            {
+                return new()
+                {
+                    new GMParamInfo(EGMParamType.Enum, "容器", "ET.KnapsackContainerType"),
+                    new GMParamInfo(EGMParamType.Int, "ItemId")
+                };
+            }
+
+            public async ETTask<bool> Run(Scene root, ParamVo paramVo)
+            {
+                var paramEnum = paramVo.Get<KnapsackContainerType>(0);
+                var paramInt = paramVo.Get<int>(1);
+
+                await KnapsackHelper.RequestAddItem(root, paramEnum, paramInt);
+
+                await ETTask.CompletedTask;
+                return true;
+            }
+        }
+
+        [GM(EGMType.Test, 1, "背包测试-删除道具")]
+        public class GM_Command_Knapsack2 : IGMCommand
+        {
+            public List<GMParamInfo> GetParams()
+            {
+                return new()
+                {
+                    new GMParamInfo(EGMParamType.Enum, "容器", "ET.KnapsackContainerType"),
+                    new GMParamInfo(EGMParamType.Long, "ItemId"),
+                };
+            }
+
+            public async ETTask<bool> Run(Scene root, ParamVo paramVo)
+            {
+                var paramEnum = paramVo.Get<KnapsackContainerType>(0);
+                var paramLong = paramVo.Get<long>(1);
+
+                await KnapsackHelper.RequestRemoveItem(root, paramEnum, paramLong);
+
+                await ETTask.CompletedTask;
+                return true;
+            }
+        }
+
+        # endregion
+
+        # region 史莱姆
+
+        [GM(EGMType.Test, 1, "史莱姆测试-添加史莱姆")]
+        public class GM_Command_Slime1 : IGMCommand
+        {
+            public List<GMParamInfo> GetParams()
+            {
+                return new()
+                {
+                    new GMParamInfo(EGMParamType.Int, "ConfigId"),
+                };
+            }
+
+            public async ETTask<bool> Run(Scene root, ParamVo paramVo)
+            {
+                var paramInt = paramVo.Get<int>();
+
+                await SlimeHelper.RequestAddSlime(root, paramInt);
+
+                await ETTask.CompletedTask;
+                return true;
+            }
+        }
+
+        [GM(EGMType.Test, 1, "史莱姆测试-删除史莱姆")]
+        public class GM_Command_Slime2 : IGMCommand
+        {
+            public List<GMParamInfo> GetParams()
+            {
+                return new()
+                {
+                    new GMParamInfo(EGMParamType.Long, "SlimeId"),
+                };
+            }
+
+            public async ETTask<bool> Run(Scene root, ParamVo paramVo)
+            {
+                var paramLong = paramVo.Get<long>();
+
+                await SlimeHelper.RequestRemoveSlime(root, paramLong);
+
+                await ETTask.CompletedTask;
+                return true;
+            }
+        }
+
+        # endregion
     }
 }
