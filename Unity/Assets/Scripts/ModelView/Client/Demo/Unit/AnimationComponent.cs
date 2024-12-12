fcfee0bef37dfb27f799af967e65b763625a9fc7
FortuneCat0v0 <1142094319@qq.com>
2024-12-12 14:23:54 +0800

    程序集合添加Animancer的引用，直接引用若是没有用，点击Unity菜单 -> Edit -> Preferences -> External Tools，点击下拉框'External ScriptEditor'选择Rider，Generate .csproj files for全部不要勾选。好像只有引用Packages中的才有效

--- Unity/Assets/Scripts/ModelView/Client/Demo/Unit/AnimationComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/Demo/Unit/AnimationComponent.cs
@@ -1,12 +1,11 @@
-﻿// using System.Collections.Generic;
-// using Animancer;
+﻿using Animancer;
 
 namespace ET.Client
 {
     [ComponentOf]
     public class AnimationComponent : Entity, IAwake, IDestroy
     {
-        // public AnimancerComponent Animancer;
+        public AnimancerComponent Animancer;
         // public AnimGroup AnimGroup;
         // public string CurrentAnimation { get; set; }
     }
